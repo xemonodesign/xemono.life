a5629d051c9bfca4148619cb238486129dd51507
Torii Megumi <torii.megumi@gmail.com>
2019-03-23 16:10:13 +0900

    LightBoxを導入し、イラストを大きく見るボタンを入れる

--- src/Header.js
+++ src/Header.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import './App.css';
 import Navi from './Navi'
-
+import LBImage from './LBImage'
 
 const Right = (props) => (
     <div className="right">{props.children}</div>
 )
 
-const Left = (props) => (
-    <div className="Left">{props.children}</div>
+const BottomLeft = (props) => (
+    <div className="bottom-left">{props.children}</div>
 )
 
 
 
 const Header = (props) => (
-    <header className={`theme-black layout-box`}>
+    <header className={`theme-black layout-box flex-column`}>
         <Navi />
         <p className='text-large bold'>使いやすくてかっこいい道具を作る<br />
             Design Studio Xemono
         </p>
-        
+        <BottomLeft>
+            <LBImage />
+        </BottomLeft>
     </header>
 )
 
