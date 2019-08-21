7296b3b65465df8e558863803b4ec0a09eb07635
Torii Megumi <torii.megumi@gmail.com>
2019-08-21 13:19:26 +0900

    ニラメッコの動画を追加する

--- src/components/ContentBox.js
+++ src/components/ContentBox.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./../App.css";
 
 const ContentBox = props => (
-  <section className={`theme-${props.theme} layout-box`}>
+  <section className={`theme-${props.theme} layout-box ${props.text ? 'text-box' : ''}`}>
     {props.children}
   </section>
 );
